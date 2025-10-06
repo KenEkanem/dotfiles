6baf206cbe7b5eb25737a08ede65f0bcbb13cdf3
KenEkanem <kennedyekanem@gmail.com>
2025-10-06 04:42:54 +0100

    06|10|2025

--- windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-Expression (&starship init powershell)
-
-Function lsd { & 'C:\ProgramData\chocolatey\bin\lsd.exe' -ah --color=always }
-
-Remove-Alias ls -Force
-Set-Alias -Name ls -Value lsd
-
